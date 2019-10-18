93084f04a34b050963907649884c76085908c770
James Tauber <jtauber@jtauber.com>
2019-10-18 04:55:43 -0400

    generalised things by introducing Suite class

--- base.py
+++ base.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Plugin:
 
     def __init__(self, error_callback, config):
@@ -12,3 +15,29 @@ class Plugin:
 
     def validate_last_line(self, filename, line_num, line):
         pass
+
+
+def error_callback(filename, line_num, error):
+    print(f"{filename}:{line_num}:{error}", file=sys.stderr)
+
+
+class Suite:
+
+    def __init__(self):
+        self.plugins = []
+
+    def add_plugin(self, module, config):
+        self.plugins.append(module.plugin(error_callback, config))
+
+    def validate_files(self, filenames):
+        for filename in filenames:
+            with open(filename, "rb") as f:
+                line_num = 0
+                for line in f:
+                    line_num += 1
+                    for plugin in self.plugins:
+                        if line_num == 1:
+                            plugin.validate_first_line(filename, line_num, line)
+                        plugin.validate_line(filename, line_num, line)
+                for plugin in self.plugins:
+                    plugin.validate_last_line(filename, line_num, line)
--- test.py
+++ test.py
@@ -1,39 +1,21 @@
 #!/usr/bin/env python3
 
 import glob
-import sys
+
+from base import Suite
 
 import plugins.whitespace
 import plugins.unicode
 
-
-def error_callback(filename, line_num, error):
-    print(f"{filename}:{line_num}:{error}", file=sys.stderr)
-
-
-PLUGINS = [
-    plugins.whitespace.plugin(error_callback, {
-        "CHECK_CRLF": True,
-        "CHECK_TABS": True,
-        "CHECK_TRAILING_WHITESPACE": True,
-        "CHECK_NO_EOF_NEWLINE": True,
-    }),
-    plugins.unicode.plugin(error_callback, {
-        "CONFIRM_UTF_8_NFD": True,
-        "CONFIRM_UTF_8_NFC": True,
-    })
-]
-
-
-for filename in sorted(glob.glob("tests/*.txt")):
-    with open(filename, "rb") as f:
-
-        line_num = 0
-        for line in f:
-            line_num += 1
-            for plugin in PLUGINS:
-                if line_num == 1:
-                    plugin.validate_first_line(filename, line_num, line)
-                plugin.validate_line(filename, line_num, line)
-        for plugin in PLUGINS:
-            plugin.validate_last_line(filename, line_num, line)
+suite = Suite()
+suite.add_plugin(plugins.whitespace, {
+    "CHECK_CRLF": True,
+    "CHECK_TABS": True,
+    "CHECK_TRAILING_WHITESPACE": True,
+    "CHECK_NO_EOF_NEWLINE": True,
+})
+suite.add_plugin(plugins.unicode, {
+    "CONFIRM_UTF_8_NFD": True,
+    "CONFIRM_UTF_8_NFC": True,
+})
+suite.validate_files(sorted(glob.glob("tests/*.txt")))
