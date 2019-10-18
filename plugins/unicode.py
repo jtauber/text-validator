6876fcf1c5a94feb5c4b97fb0953f7015a3fb874
James Tauber <jtauber@jtauber.com>
2019-10-18 17:40:59 -0400

    refactored error callback calling

--- plugins/unicode.py
+++ plugins/unicode.py
@@ -7,15 +7,18 @@ class Unicode(Plugin):
 
     def validate_line(self, filename, line_num, line):
 
+        def error(message):
+            self.error_callback(filename, line_num, message)
+
         utf_8_line = line.decode("utf-8")
 
         if self.config.get("CONFIRM_UTF_8_NFC"):
             if utf_8_line != unicodedata.normalize("NFC", utf_8_line):
-                self.error_callback(filename, line_num, "not NFC")
+                error("not NFC")
 
         if self.config.get("CONFIRM_UTF_8_NFD"):
             if utf_8_line != unicodedata.normalize("NFD", utf_8_line):
-                self.error_callback(filename, line_num, "not NFD")
+                error("not NFD")
 
 
 plugin = Unicode
--- plugins/whitespace.py
+++ plugins/whitespace.py
@@ -5,22 +5,29 @@ class Whitespace(Plugin):
 
     def validate_line(self, filename, line_num, line):
 
+        def error(message):
+            self.error_callback(filename, line_num, message)
+
         if self.config.get("CHECK_CRLF"):
             if line.endswith(b"\r\n"):
-                self.error_callback(filename, line_num, "line ends with CRLF")
+                error("line ends with CRLF")
 
         if self.config.get("CHECK_TABS"):
             if b"\t" in line:
-                self.error_callback(filename, line_num, "line contains a tab")
+                error("line contains a tab")
 
         if self.config.get("CHECK_TRAILING_WHITESPACE"):
             if line.rstrip(b"\n").endswith((b" ", b"\t")):
-                self.error_callback(filename, line_num, "trailing whitespace")
+                error("trailing whitespace")
 
     def validate_last_line(self, filename, line_num, line):
+
+        def error(message):
+            self.error_callback(filename, line_num, message)
+
         if self.config.get("CHECK_NO_EOF_NEWLINE"):
             if not line.endswith(b"\n"):
-                self.error_callback(filename, line_num, "no newline at end of file")
+                error("no newline at end of file")
 
 
 plugin = Whitespace
